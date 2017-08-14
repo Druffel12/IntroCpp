06e1c659a095b352ba1a388b10dc033339509b46
s179052 <s179052@SEA-8D595E>
2017-08-14 12:45:57 -0700

    Add project files.

---
+++ 1-HelloAIE/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+
+	std::cout << "Hello Aie" << std::endl;
+	std::cout << "My Favorite tracks are" << std::endl;
+	std::cout << "1. way down we go by Kaleo" << std::endl;
+	std::cout << "2. Alan Walker Fade NCS" << std::endl;
+	std::cout << "3. Sandcastle MrSuicideSheep" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	
+	//Variable count out NF
+	int a, b;
+	int result;
+
+	a = -1;
+	result = a + 1;
+	std::cout << result;
+
+	system("pause");
+
+}
